aa7bc02f96286bc84687113b5556016f4426ac31
xxy <1152024415@qq.com>
2022-09-02 21:03:30 -0700

    master

---
+++ GAMESERVER/log.h
@@ -0,0 +1,27 @@
+#ifndef __GAMESERVER__
+#define __GAMESERVER__
+
+#include <string>
+
+namespace gameserver{
+
+//存放字段属性
+class LogEvent{
+
+};
+
+class Logger{
+
+};
+// 输出
+class LogHandler{
+
+};
+
+class LogFormatter{
+
+};
+
+}
+
+#endif
---
+++ README.md
@@ -0,0 +1,15 @@
+# webserver
+Ubuntu 20.04
+
+gcc 9.4.0
+
+cmake
+
+## 日志
+1.  Log4j
+
+    Logger(定义类别)
+        |
+        |---Formatter
+        |
+    Appender(handler)
